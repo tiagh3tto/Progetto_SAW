1d11f18064b74ef98f876ceb061314bd9f4f0d61
tiagh3tto <tiagh3tto@gmail.com>
2024-01-18 12:16:35 +0100

    Added dropdownlist to home page

--- components/body_start.php
+++ components/body_start.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
         <link rel="stylesheet" href="../assets/styles/styles.css">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
         <title>Re-View</title>
     </head>
     <body>
--- components/navbar/navbar.php
+++ components/navbar/navbar.php
@@ -1,13 +1,24 @@
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <a class="navbar-brand" href="#"><img src="../assets/img/logo3.png" alt="Logo"></a>
-            <div class="collapse navbar-collapse">
-            <ul class="navbar-nav">
-                <li class="nav-item">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="#"><img src="../assets/img/logo2.png" alt="Logo"></a>
+    <div class="collapse navbar-collapse">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
                 <a class="nav-link" href="#">Home</a>
-                </li>
-                <li class="nav-item">
+            </li>
+            <li class="nav-item">
                 <a class="nav-link" href="#">Catalogo</a>
-                </li>
-            </ul>
-            </div>
-        </nav>
+            </li>
+        </ul>
+        <ul class="navbar-nav">
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <i class="fas fa-user"></i>
+                </a>
+                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">Registrati</a>
+                    <a class="dropdown-item" href="#">Accedi</a>
+                </div>
+            </li>
+        </ul>
+    </div>
+</nav>
