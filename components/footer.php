47475600faa80a48135050a6fc62a2b6f7e45a86
“Massi1399” <massi9.1.9.3@gmail.com>
2024-01-19 12:30:56 +0100

    Added .gitignore file, added js script in footer.php, modified reg-form.php

--- components/footer.php
+++ components/footer.php
@@ -6,5 +6,26 @@
 
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+        <script>
+        // Example starter JavaScript for disabling form submissions if there are invalid fields
+        (() => {
+        'use strict'
+
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        const forms = document.querySelectorAll('.needs-validation')
+
+        // Loop over them and prevent submission
+        Array.from(forms).forEach(form => {
+            form.addEventListener('submit', event => {
+            if (!form.checkValidity()) {
+                event.preventDefault()
+                event.stopPropagation()
+            }
+
+            form.classList.add('was-validated')
+            }, false)
+        })
+        })();
+    </script>
     </body>
 </html>
--- private/reg-form.php
+++ private/reg-form.php
@@ -1,5 +1,6 @@
-<form class="row g-3 needs-validation" novalidate>
-  <div class="col-md-4">
+<div style='display: flex; justify-content: center; align-items: center; height: 100vh;'>
+    <form class='row g-3 needs-validation' novalidate style="width: 50%">
+    <div class="col-md-4">
     <label for="validationCustom01" class="form-label">First name</label>
     <input type="text" class="form-control" id="validationCustom01" value="Mark" required>
     <div class="valid-feedback">
@@ -14,15 +15,46 @@
     </div>
   </div>
   <div class="col-md-4">
-    <label for="validationCustomUsername" class="form-label">Username</label>
+    <label for="validationCustomEmail" class="form-label">Email</label>
     <div class="input-group has-validation">
-      <span class="input-group-text" id="inputGroupPrepend">@</span>
-      <input type="text" class="form-control" id="validationCustomUsername" aria-describedby="inputGroupPrepend" required>
+      <input type="email" class="form-control" id="validationCustomEmail" aria-describedby="inputGroupPrepend" required>
       <div class="invalid-feedback">
-        Please choose a username.
+        Please enter a valid email address.
       </div>
     </div>
   </div>
+  <div class="row g-3 align-items-center">
+  <div class="col-auto">
+    <label for="inputPassword6" class="col-form-label">Password</label>
+  </div>
+  <div class="col-auto">
+    <input type="password" id="inputPassword6" class="form-control" aria-describedby="passwordHelpInline" minlength="8">
+  </div>
+  <div class="col-auto">
+    <span id="passwordHelpInline" class="form-text">
+      Must be at least 8 characters long.
+    </span>
+  </div>
+</div>
+  <div class="col-12">
+    <div class="form-check">
+      <input class="form-check-input" type="checkbox" value="" id="invalidCheck" required>
+      <label class="form-check-label" for="invalidCheck">
+        Agree to terms and conditions
+      </label>
+      <div class="invalid-feedback">
+        You must agree before submitting.
+      </div>
+    </div>
+  </div>
+ <div class="col-12">
+    <button class="btn btn-primary" type="submit">Submit form</button>
+  </div>
+    </form>
+</div>
+
+<!--
+<form class="row g-3 needs-validation" novalidate>
   <div class="col-md-6">
     <label for="validationCustom03" class="form-label">City</label>
     <input type="text" class="form-control" id="validationCustom03" required>
@@ -47,18 +79,4 @@
       Please provide a valid zip.
     </div>
   </div>
-  <div class="col-12">
-    <div class="form-check">
-      <input class="form-check-input" type="checkbox" value="" id="invalidCheck" required>
-      <label class="form-check-label" for="invalidCheck">
-        Agree to terms and conditions
-      </label>
-      <div class="invalid-feedback">
-        You must agree before submitting.
-      </div>
-    </div>
-  </div>
-  <div class="col-12">
-    <button class="btn btn-primary" type="submit">Submit form</button>
-  </div>
-</form>
+</form>-->
