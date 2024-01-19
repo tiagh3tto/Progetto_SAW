2d038e477806d08820165e26bdc6c9962cd67bc3
tiagh3tto <tiagh3tto@gmail.com>
2024-01-19 12:18:16 +0100

    Renaming of files, Added log-form

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
-include("../components/body_start.php");
+include("../components/head.php");
 include("../components/navbar/navbar.php");
-include("../components/main/main.php");
+include("../components/main/home.php");
 include("../components/footer.php");
 ?>
