2c3d9b4b53c0ed9d574024879008556a171a2ce2
tiagh3tto <tiagh3tto@gmail.com>
2024-01-19 10:14:21 +0100

    DB_CREDENTIALS BECAME PHP FILE

--- private/connection.php
+++ private/connection.php
@@ -1,29 +1,7 @@
 <?php
-	$filename = $_SERVER['DOCUMENT_ROOT']."/SAW/Progetto_SAW/private/db_credentials.txt";
-	if (!$fp = fopen($filename, 'r')) {
-		echo "Impossibile aprire il file ($filename)";										//OJO: gestione errori
-		exit;
-	}
-	if ($fp) {
-		flock($fp, LOCK_SH);
-		if (($line = fgets($fp, filesize($filename))) !== FALSE) {
-			list($server_name, $db_usr, $db_pwd, $db_name) = explode("\t", $line);
-			trim($db_name);
-			trim($db_pwd);																	//OJO: dovremmo hashare la pwd nel file delle credenziali????	
-			trim($db_usr);
-			}		
-		else{
-			echo "Errore: la funzione fgets() è fallita inaspettatamente\n";				//OJO: gestione errori
-			exit;
-		}
-		flock($fp, LOCK_UN);
-		fclose($fp);		
-	}
-	else {
-    	echo "Il file ($filename) non ha i permessi di lettura";							//OJO: gestione errori
-	}
+	include($_SERVER['DOCUMENT_ROOT']."/SAW/Progetto_SAW/private/db_credentials.php");
 
-	$con = mysqli_connect($server_name, $db_usr ,$db_pwd , $db_name);
+	$con = mysqli_connect(DB_HOST, DB_USER , DB_PASS, DB_NAME);
 	if (mysqli_connect_errno()) {
 		error_log("Failed to connect to MySQL: " . mysqli_connect_error(),3, $_SERVER['DOCUMENT_ROOT']."/SAW/Progetto_SAW/private/logs/errors.log");		//OJO: gestione errori
 		echo "Impossibile collegarsi al Database";
---
+++ private/db_credentials.php
@@ -0,0 +1,6 @@
+<?php
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'root');
+    define('DB_PASS', 'Saw20Tiago23');
+    define('DB_NAME', 'saw_2023');
+?>
