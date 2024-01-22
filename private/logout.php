971aafeee5cade4615bb402eadf6d842bae278fc
tiagh3tto <tiagh3tto@gmail.com>
2024-01-22 14:57:41 +0100

    Adding logout.php

---
+++ private/logout.php
@@ -0,0 +1,11 @@
+<?php
+    session_start();   
+    $_SESSION = [];
+    
+    if (isset($_COOKIE[session_name()])) {
+        setcookie(session_name(), '', time()-86400, '/');
+    }
+    session_destroy();
+    header('Location: /SAW/Progetto_SAW/private/login.php');
+    exit;
+?>
