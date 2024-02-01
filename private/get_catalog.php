097f6191a75d861c706886dcfbec7c107a4ebb22
“Massi1399” <massi9.1.9.3@gmail.com>
2024-02-01 21:43:00 +0100

    update catalog table

--- private/get_catalog.php
+++ private/get_catalog.php
@@ -1,4 +1,5 @@
 <?php
+//se viene chiamato get_catalog dalla searchbar
 if((isset($_GET["searchBar"]) && isset($_GET["filter"])) && (!empty($_GET["searchBar"]) && !empty($_GET["filter"]))){
     try{
         include($_SERVER['DOCUMENT_ROOT']."/SAW/Progetto_SAW/private/connection.php");
@@ -62,9 +63,22 @@ else{
         $query = "SELECT * FROM film;";
         $res = mysqli_query($con, $query);
         $count = mysqli_num_rows($res);
+        
+       /* include($_SERVER['DOCUMENT_ROOT']."/SAW/Progetto_SAW/private/db_credentials.php");
+        $con2 = mysqli_connect(DB_HOST, DB_USER , DB_PASS, DB_NAME);
+        if (mysqli_connect_errno()) {
+            error_log("Failed to connect to MySQL: " . mysqli_connect_error(),3, $_SERVER['DOCUMENT_ROOT']."/SAW/Progetto_SAW/private/logs/errors.log");		//OJO: gestione errori
+            echo "Impossibile collegarsi al Database";
+            exit;
+        }*/
         //build data array
         if($count > 0){
             while(($row = mysqli_fetch_assoc($res)) != NULL){
+                
+                //else
+                    //$review = NULL;    
+                //mysqli_free_result($reviews_res);
+                //mysqli_stmt_close($reviews_stmt);
                 $data[] = [
                     "nome"=>$row["Nome"],
                     "genere"=>$row["Genere"],
@@ -74,11 +88,15 @@ else{
                     //"trama"=>$row["Trama"],
                     "img"=>$row["Img"],
                     "durata"=>$row["Durata"],
-                    "casa_produzione"=>$row["Casa_Produzione"]
+                    "casa_produzione"=>$row["Casa_Produzione"],
+                    "gradimento"=> ""
                 ];
             }
+            // mysqli_free_result($res);
+            //mysqli_stmt_close($stmt);
             //return JSON formatted data
             echo(json_encode($data));
+           
         }
         else
             echo "Impossibile caricare il catalogo";
