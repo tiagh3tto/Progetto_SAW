f29dbfb1bdddfabac2360026ea2031184d2a091b
tiagh3tto <tiagh3tto@gmail.com>
2024-02-04 16:25:54 +0100

    ADD_FILM

--- private/ban_users.php
+++ private/ban_users.php
@@ -1,8 +1,4 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-// Include your database connection file here
 try{
     include($_SERVER['DOCUMENT_ROOT']."/SAW/Progetto_SAW/private/connection.php");
 
