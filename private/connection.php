4387a0c15e581d3f6eb1e7a060c0deaef0b1e21f
“Massi1399” <massi9.1.9.3@gmail.com>
2024-02-16 09:56:37 +0100

    added mysqli_report to connection.php and updated database_error.html message

--- private/connection.php
+++ private/connection.php
@@ -1,6 +1,6 @@
 <?php
 	include($_SERVER['DOCUMENT_ROOT']."/SAW/Progetto_SAW/private/db_credentials.php");
-
+	mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 	$con = mysqli_connect(DB_HOST, DB_USER , DB_PASS, DB_NAME);
 	if (mysqli_connect_errno()) {
 		error_log("Failed to connect to MySQL: " . mysqli_connect_error(),3, $_SERVER['DOCUMENT_ROOT']."/SAW/Progetto_SAW/private/logs/errors.log");		//OJO: gestione errori
